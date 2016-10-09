896786085794042266d15f8fc25eb0c35a9086a2
McSim <mcsimest@gmail.com>
2016-10-09 18:12:17 +0300

    Неверное решение
    Решение до того, как стало понятно, что иеется в виду сортировка уже
    просмотренных товаров для рекомендации.

--- MIPT-K5-W4-MP.py
+++ MIPT-K5-W4-MP.py
@@ -7,6 +7,7 @@ Created on Thu Oct 06 22:32:26 2016
 
 import csv
 import pandas as pd
+import numpy as np
 
 train_views = []
 train_buys = []
@@ -29,22 +30,38 @@ merged_train_buys = []
 for i in train_buys:
     merged_train_buys += i
 
-del(i)
-
 from collections import Counter
 counter_views = Counter(merged_train_views)
 counter_buys =  Counter(merged_train_buys)
 #index=[x[0] for x in counter_views.most_common()]
        
-df_train_views = pd.DataFrame(counter_views.most_common(), columns=("session_id","views"))
-df_train_buys = pd.DataFrame(counter_buys.most_common(), columns=("session_id","buys"))
-df_train = pd.merge(df_train_views, df_train_buys, on='session_id', how='outer')
-df_train = df_train.set_index("session_id")
+df_train_views = pd.DataFrame(counter_views.most_common(), columns=("goods_id","views"))
+df_train_buys = pd.DataFrame(counter_buys.most_common(), columns=("goods_id","buys"))
+df_train = pd.merge(df_train_views, df_train_buys, on='goods_id', how='outer')
+df_train = df_train.set_index("goods_id")
 del(df_train_views, df_train_buys, counter_views, counter_buys)
-#df_train.sort(['buys','views'], ascending=[False, False], inplace=True)
-df_train.sort_values(['buys','views'], ascending=[False, False], inplace=True)
+df_train.sort_values('views', ascending=False, inplace=True)
+
+train1views = df_train.index[:1]
+train5views = df_train.index[:5]
+
+df_train.sort_values('buys', ascending=False, inplace=True)
+i = 1
+while len(df_train.buys[:i].unique()) < 6 :
+    i += 1
+view_order = [merged_train_views.index(df_train.index[j]) for j in range(i-1)]
+view_order = view_order + list(np.zeros(len(df_train)-j-1))
+del(i,j)
+df_train['view_order'] = pd.Series(view_order, index=df_train.index)
+df_train.sort_values(['buys','view_order'], ascending=[False, True], inplace=True)
+
+train1buys = df_train.index[:1]
+train5buys = df_train.index[:5]
+
+train_buys = filter(lambda a: a != [], train_buys)
+AveragePrecision_1 = np.mean([x.count(train1buys) for x in train_buys])
 
 #AverageRecall_1
-#AveragePrecision_1
+
 #AverageRecall_5
 #AveragePrecision_5
