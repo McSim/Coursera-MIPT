6142d6845d57204e82675cfa119e9dc4a0596761
McSim <mcsimest@gmail.com>
2016-10-09 19:58:36 +0300

    После корректировки задачи
    Переделка кода. Задача понята была неверно.

--- MIPT-K5-W4-MP.py
+++ MIPT-K5-W4-MP.py
@@ -15,7 +15,7 @@ with open("coursera_sessions_train.txt", "r") as train_file:
     reader=csv.reader(train_file,delimiter=';')
     for a, b in reader:
         if b == "":
-            train_buys.append([])
+            train_buys.append('')
         else:
             train_buys.append([int(x) for x in b.split(',')])
         train_views.append([int(x) for x in a.split(',')])
@@ -28,8 +28,10 @@ for i in train_views:
     
 merged_train_buys = []
 for i in train_buys:
-    merged_train_buys += i
+        merged_train_buys += i
 
+del(i)
+    
 from collections import Counter
 counter_views = Counter(merged_train_views)
 counter_buys =  Counter(merged_train_buys)
@@ -37,31 +39,27 @@ counter_buys =  Counter(merged_train_buys)
        
 df_train_views = pd.DataFrame(counter_views.most_common(), columns=("goods_id","views"))
 df_train_buys = pd.DataFrame(counter_buys.most_common(), columns=("goods_id","buys"))
-df_train = pd.merge(df_train_views, df_train_buys, on='goods_id', how='outer')
-df_train = df_train.set_index("goods_id")
+df_freq_train = pd.merge(df_train_views, df_train_buys, on='goods_id', how='outer')
+df_freq_train = df_freq_train.set_index("goods_id")
+df_freq_train.sort_values('views', ascending=False, inplace=True)
 del(df_train_views, df_train_buys, counter_views, counter_buys)
-df_train.sort_values('views', ascending=False, inplace=True)
 
-train1views = df_train.index[:1]
-train5views = df_train.index[:5]
+df_train = pd.DataFrame({'views' : train_views, 'buys' : train_buys})
+df_train = df_train[df_train.buys != '']
+srtd_views_by_views = []
+for x in df_train.views:
+    df_x = pd.DataFrame(np.unique(x), columns=["id"])
+    df_x.loc[:,'order'] = list(range(len(np.unique(x))))
+    df_x.loc[:,'freq'] = [df_freq_train.buys[y] for y in np.unique(x)]
+    df_x.sort_values(['freq', 'order'], ascending=[False,True], inplace=True)
+    srtd_views_by_views.append(list(df_x.id.values))
+df_train['sorted_views'] = pd.Series(srtd_views_by_views, index=df_train.index)
 
-df_train.sort_values('buys', ascending=False, inplace=True)
-i = 1
-while len(df_train.buys[:i].unique()) < 6 :
-    i += 1
-view_order = [merged_train_views.index(df_train.index[j]) for j in range(i-1)]
-view_order = view_order + list(np.zeros(len(df_train)-j-1))
-del(i,j)
-df_train['view_order'] = pd.Series(view_order, index=df_train.index)
-df_train.sort_values(['buys','view_order'], ascending=[False, True], inplace=True)
+# в результате имеем датафрейм с просмотрами, покупками и просмотрами отсортированными по частоте просмотров и порядку просмотра
 
-train1buys = df_train.index[:1]
-train5buys = df_train.index[:5]
 
-train_buys = filter(lambda a: a != [], train_buys)
-AveragePrecision_1 = np.mean([x.count(train1buys) for x in train_buys])
 
+#AveragePrecision_1
 #AverageRecall_1
-
 #AverageRecall_5
 #AveragePrecision_5
